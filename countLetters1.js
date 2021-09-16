45d062463163b2fdb2f5ede6f2eb25da117a242f
Sarada <sarada@ranjits-mbp.lan>
2021-09-16 15:49:43 -0400

    lotide programms

--- countLetters1.js
+++ countLetters1.js
@@ -1,29 +1,30 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-  
-  function countLetters (phrase) {
-    const charsObj = {}; 
-    const letters = phrase.split('');
-    for (let index = 0; index < letters.length; index ++) {
-        // const count = 1;
-        const character = letters[index];
-        if (character !== ' ') {
-            if (charsObj[character]) {
-                charsObj[character] = charsObj[character] + 1;
-            } else {
-               charsObj[character] = 1;             
-            }   
-        }
-        
-    }
-    return charsObj.h;
-    
-}
-console.log(countLetters('sarada'));
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
-//assertEqual(countLetters("sarada"));
+function countLetters (phrase) {
+  const charsObj = {}; 
+  const letters = phrase.split('');
+  for (let index = 0; index < letters.length; index ++) {
+      // const count = 1;
+      const character = letters[index];
+      if (character !== ' ') {
+          if (charsObj[character]) {
+              charsObj[character] = charsObj[character] + 1;
+          } else {
+             charsObj[character] = 1;             
+          }   
+      }
+      
+  }
+  return charsObj.h;
+  
+}
+
+module.exports = countLetters;
+console.log(assertEqual(countLetters("lighthouse in the house"),4));
+console.log(countLetters("lighthouse in the house"));
